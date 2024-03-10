b39be04800d2a418177c3bb33bd9a19e22090eb6
AshtonB08 <93924688+AshtonB08@users.noreply.github.com>
2024-03-10 13:12:50 +0000

    options scene rough set up done (except volume slider), all buttons work. no way to test the quit button without building the game but debug log says it works so im trusting it lmao. tldr; menu buttons done options scene needs a volume slider game needs sound to be able to put that in >:-)

--- vampgame/Assets/Scripts/Menu.cs
+++ vampgame/Assets/Scripts/Menu.cs
@@ -9,19 +9,30 @@ public class Menu : MonoBehaviour
 
 
 	/*	FUNCTIONS	*/
+
+
+	//using scene index instead of name, can be changed if using numbers becomes confusing
 	public void StartButton()
 	{
 		SceneManager.LoadScene(1); 
+		Debug.Log ("Start Game");
 	}
 
 	public void OptionsButton()
 	{
-		//SceneManager.LoadScene(2);
+		SceneManager.LoadScene(2);
+		Debug.Log("Options");
 	}
 
+	public void HomeButton()
+	{
+		SceneManager.LoadScene(0);
+		Debug.Log("Home");
+	}
 	public void QuitButton()
 	{
 		Application.Quit();
+		Debug.Log("Quit");
 	}
 
 }
