9cd492b99eba2d885c22b2b7ee812b44870341d7
AshtonB08 <93924688+AshtonB08@users.noreply.github.com>
2024-04-15 20:28:41 +0100

    the light can shrink and grow. set to button inputs at the minute for testing but they do work over time aswell. need to add pickup function

---
+++ vampgame/Assets/Scripts/Player/LightRing.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LightRing : MonoBehaviour
+{
+
+    public GameObject Lantern; 
+    public float shrinkRate = 0.1f; //how fast the light shrinks
+    public float growRate = 0.1f; //how fast the light can grow
+
+    public Vector3 minSize = new Vector3(0.1f, 0.1f, 0.1f); // min size is small enough that you cant see it
+    public Vector3 maxSize = new Vector3(2.5f, 2.5f, 2.5f); //set max size as slightly bigger than initial size
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L) )
+        {
+            if (transform.localScale != minSize)
+            {
+                Transform transform = Lantern.transform;
+                
+                //shrinks the lanterns scale uniformly
+                transform.localScale -= new Vector3(shrinkRate, shrinkRate, shrinkRate);
+
+                //clamping the minimum size
+                transform.localScale = new Vector3(
+                Mathf.Clamp(transform.localScale.x, minSize.x, maxSize.x),
+                Mathf.Clamp(transform.localScale.y, minSize.y, maxSize.y),
+                Mathf.Clamp(transform.localScale.z, minSize.z, maxSize.z));
+
+			}
+        }
+
+		if (Input.GetKey(KeyCode.K))
+		{
+			if (transform.localScale != maxSize)
+			{
+				Transform transform = Lantern.transform;
+
+				//grows the lanterns scale uniformly
+				transform.localScale += new Vector3(growRate, growRate, growRate);
+
+				//clamping the maximum size
+				transform.localScale = new Vector3(
+				Mathf.Clamp(transform.localScale.x, minSize.x, maxSize.x),
+				Mathf.Clamp(transform.localScale.y, minSize.y, maxSize.y),
+				Mathf.Clamp(transform.localScale.z, minSize.z, maxSize.z));
+
+			}
+		}
+	}
+}
